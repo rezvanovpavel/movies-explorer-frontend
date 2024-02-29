afaf0b7e1ac26fc359a3d0f5381af660e22c1d33
Pavel Rezvanov <rezvanoffpavel@yandex.ru>
2024-03-01 00:08:41 +0300

    первый коммит

--- movies-frontend/src/components/AboutMe/AboutMe.js
+++ movies-frontend/src/components/AboutMe/AboutMe.js
@@ -9,8 +9,7 @@ function AboutMe() {
     <div className="aboutme__container">
         <h3 className="aboutme__heading-name">Павел</h3>
         <h4 className="aboutme__heading-profession">Фронтенд-разработчик, 23 года</h4>
-        <p className="aboutme__text">Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. 
-С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+        <p className="aboutme__text">Я живу в Москве, заканчиваю факультет ИФ ПМГМУ. Женат. Я люблю программировать и писать стихи. Работаю в лаборатории биоинформатики и фармакологического моделирования, занимаюсь Data Science и Веб-разработкой. Мечтаю стать настоящим профессионалом своего дела.</p>
         <a className="aboutme__link" href="https://github.com/rezvanovpavel" target="_blank" rel="noreferrer">Github</a>
     </div>
     <img className="aboutme__avatar" src={avatar} alt="Аватар"></img>
